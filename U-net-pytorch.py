2b6c502dd4a8a00e60b6df7e3429bfeee9a3d9c2
DMAN <32004742+Dmaner@users.noreply.github.com>
2018-09-02 21:58:36 +0800

    Update U-net-pytorch.py

--- U-net-pytorch.py
+++ U-net-pytorch.py
@@ -3,6 +3,8 @@ References: https://github.com/milesial/Pytorch-UNet
 Train dataset: BSD 500
 '''
 import torch.nn as nn
+import torch
+import torch.nn.functional as F
 
 class Double_conv(nn.Module):
     '''(conv => BN => ReLU) * 2'''
@@ -22,20 +24,63 @@ class Double_conv(nn.Module):
         return x
 
 class Down_sample(nn.Module):
-    def __init__(self, in_ch, out_ch):
+    def __init__(self, in_channel, out_channel):
         super(Down_sample, self).__init__()
         self.layer = nn.Sequential(
             nn.MaxPool2d(2),
-            Double_conv(in_ch, out_ch)
+            Double_conv(in_channel, out_channel)
         )
 
     def forward(self, x):
         x = self.layer(x)
         return x
 
-class 
+class Up_sample(nn.Module):
+    def __init__(self, in_channel, out_channel, bilinear=True):
+        super(Up_sample,self).__init__()
+        if bilinear:
+            self.layer = nn.Upsample(scale_factor=2,
+                                     mode='bilinear',
+                                     align_corners=True)
+        else:
+            self.layer = nn.ConvTranspose2d(in_channel//2,
+                                            out_channel//2,
+                                            kernel_size=2,
+                                            stride=2)
 
-class U_net(nn.Module):
-    def __init__(self):
-        super(U_net, self).__init__()
+    def forward(self, x1, x2):
+        x1 = self.layer(x1)
+        diffX = x1.size()[2] - x2.size()[2]
+        diffY = x1.size()[3] - x2.size()[3]
+        x2 = F.pad(x2, (diffX // 2, int(diffX / 2),
+                        diffY // 2, int(diffY / 2)))
+        x = torch.cat([x2, x1], dim=1)
+        x = self.conv(x)
+        return x
 
+class UNet(nn.Module):
+    def __init__(self, n_channels, n_classes):
+        super(UNet, self).__init__()
+        self.inc = Double_conv(n_channels, 64)
+        self.down1 = Down_sample(64, 128)
+        self.down2 = Down_sample(128, 256)
+        self.down3 = Down_sample(256, 512)
+        self.down4 = Down_sample(512, 512)
+        self.up1 = Up_sample(1024, 256)
+        self.up2 = Up_sample(512, 128)
+        self.up3 = Up_sample(256, 64)
+        self.up4 = Up_sample(128, 64)
+        self.outc = nn.Conv2d(64, n_classes)
+
+    def forward(self, x):
+        x1 = self.inc(x)
+        x2 = self.down1(x1)
+        x3 = self.down2(x2)
+        x4 = self.down3(x3)
+        x5 = self.down4(x4)
+        x = self.up1(x5, x4)
+        x = self.up2(x, x3)
+        x = self.up3(x, x2)
+        x = self.up4(x, x1)
+        x = self.outc(x)
+        return x
